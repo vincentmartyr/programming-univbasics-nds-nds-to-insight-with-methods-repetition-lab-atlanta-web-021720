11a2d85b341a15142cd9ab209f71da702bb4f82b
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-11 14:36:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -71,7 +71,7 @@ def total_gross(source)
 grand_total = 0
 super_index = 0
 while super_index < source.length do
-    grand_total += [directors_totals(source)[super_index]]
+    grand_total += directors_totals(source)[super_index]
     super_index += 1
 end
 grand_total
